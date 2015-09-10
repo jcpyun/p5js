34c5c82c65089c069847ff180eabdb8bad362c39
jcpyun <johnchpyun@gmail.com>
2015-09-10 00:42:23 -0400

    bridge initialize

---
+++ 1/bridge.js
@@ -0,0 +1,23 @@
+var xpos1;
+var xpos2;
+var xpos3;
+var xpos4;
+var thin = 8;
+var thick = 36;
+
+function setup() {
+  createCanvas(710, 400);
+  noStroke();
+  xpos1 = width/2;
+  xpos2 = width/2;
+  xpos3 = width/2;
+  xpos4 = width/2;
+  
+}
+
+function draw() {
+  background(50, 89, 100);
+fill(102);
+  rect(xpos2, 0, thick, height/2);
+
+}
